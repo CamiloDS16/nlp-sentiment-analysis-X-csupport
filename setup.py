54472872591c4b9d3914e1fa30f8dcea123d99b3
Camilo Durango <camilodurangos@gmail.com>
2023-11-06 16:56:39 -0500

    Initial commit with project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='NLP-based chatproject that wilwill focus on developing a chatbot (“Yoldi”) capable of understanding and answering predefined customer queries. The project will not extend to real-time dynamic interactions involving personalized customer data.',
+    author='Camilo Durango',
+    license='BSD-3',
+)
